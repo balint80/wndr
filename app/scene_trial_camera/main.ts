aac199430c9a06604a1ea02f5729bb8dd887c73c
balint80 <balint.home@gmail.com>
2024-09-01 20:53:23 +0200

    ...

--- app/scene_trial_camera/main.ts
+++ app/scene_trial_camera/main.ts
@@ -1,7 +1,15 @@
 import { EventData, Page } from '@nativescript/core';
 import { SceneTrialCamera } from './model';
 
+var app = require('application');
+
 export function onNavigatingTo(args: EventData) {
     const page = <Page>args.object;
     page.bindingContext = page.navigationContext["scene"] as SceneTrialCamera
+}
+
+export function onCreatingView(args) {
+    if (app.ios) {
+        // TODO: ...
+    }
 }
